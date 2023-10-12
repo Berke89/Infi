dca377548b1e65d4d0126f66c1c067554e3df7f4
Berke Tuncbeden <berke.tuncbeden@student.htldornbirn>
2023-10-12 15:42:09 +0200

    first commit

---
+++ src/ControlStructures3.java
@@ -0,0 +1,24 @@
+package at.ran.basics;
+
+import java.util.Random;
+public class ControlStructures3 {
+    public static void main (String[] args) {
+        // Erstelle eine Zufallszahl zwischen 0 und 100
+        Random random = new Random ();
+        int randomNumber = random.nextInt(100);
+
+        if ((randomNumber>50) && (randomNumber<90)){
+
+        } else if (randomNumber<10){
+
+        } else
+
+
+        // Gib die Zufallszahl aus
+
+
+        // Wenn die Zahl kleiner ist als 20 gib aus "Mini"
+        // Wenn die Zahl zw. 20 und 50 ist gib "Medium"
+        // Wenn die Zahl größer als 50 ist gib aus "Large"
+    }
+}
---
+++ src/New.java
@@ -0,0 +1,21 @@
+public class New {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+
+        // short - ganze zahl
+        // int - ganze zahl
+        // float - kommazahl
+        // double - kommazahl (groß)
+        // boolean - true/false
+        // char - zeichen
+        // String - zeichenkette
+
+        int number = 7;
+        String name = "Berke";
+
+        System
+
+    }
+    //strg blank
+}
